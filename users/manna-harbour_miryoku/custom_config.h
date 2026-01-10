c73c72f1a11de1280a8dac9492313fdcfacc44a8
evgnskv <evgnskv@pm.me>
2026-01-10 14:52:43 +0000

    try rm enum

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -92,20 +92,3 @@ MIRYOKU_X(DB,     "D Extra")
 #define MIRYOKU_LAYERMAPPING_CB MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_DA MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_DB MIRYOKU_MAPPING
-
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
-#define U_AA    10
-#define U_AB    11
-#define U_CA    12
-#define U_CB    13
-#define U_DA    14
-#define U_DB    15
