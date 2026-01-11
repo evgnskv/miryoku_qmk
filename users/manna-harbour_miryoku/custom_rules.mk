a1f38e2ac42e871aeea585f7a0f94ff8992cd161
evgnskv <evgnskv@pm.me>
2026-01-11 10:59:00 +0000

    enable mouse keys

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,7 +2,7 @@
 # https://github.com/manna-harbour/miryoku
 
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
